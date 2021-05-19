8ade692be6686144a8bb0bac5f30720c5de86906
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-19 03:24:34 +0200

    #35 adds qrcode

--- js/start.js
+++ js/start.js
@@ -16,6 +16,16 @@ $(function() {
     const roomid = 'test';
     socket.emit('join-room', roomid);
 
+    const QRCode = require('qrcode');
+    const stringdata = `http://192.168.10.102:3000/controller/${roomid}`;
+   
+    QRCode.toCanvas(stringdata, { errorCorrectionLevel: 'H' }, function (err, canvas) {
+        if (err) throw err
+       
+        var container = document.getElementById('qrcode')
+        container.appendChild(canvas)
+      })
+
     socket.on('allControllerActivity', function(data) {
         if(data) {
             if ($('.pointer[session_id="' + data.session_id + '"]').length <= 0) {
