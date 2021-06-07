5d9bf85d5c92652f0619de5fd0c16bf903b5f57a
Benjamin Adolph <Benjamin.Adolph@gmx.de>
2021-06-07 21:06:08 +0200

    #56 Add turtle controller

--- js/start.js
+++ js/start.js
@@ -32,9 +32,14 @@ $(function() {
         $('#start-intro').fadeOut();
     });
 
-    socket.on('allControllerActivity', function(data) {
-        console.log(data.direction);
+    socket.on('canvasMoveViewport', function(data) {
         moveViewport(data.direction)
+    });
+
+    socket.on('canvasDblClickButton', function(data) {
+        tapButton = data.clicked;
+        console.log("canvasDblClickButton" + tapButton);
+
     });
 
     var $screenButton = $('.button-intro--screen-one');
