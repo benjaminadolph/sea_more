9bc57dbf598a2b38e26552b56c0b0eea1e75db12
Benjamin Adolph <Benjamin.Adolph@gmx.de>
2021-06-14 21:55:12 +0200

    #84 Fix Bugs in Heroku

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const indexRoute = require('./routes/index');
 
 const app = express();
@@ -7,7 +8,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 const io = require('socket.io')(app.listen(port));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.dirname(require.main.filename) + '/public'));
 app.use('/', indexRoute);
 app.set('view engine', 'ejs');
 
